162b8c37aef22d44a469f86e094f7dea62bbdb2e
KaegeneTeo <Kaegenie@gmail.com>
2023-10-19 20:23:49 +0800

    profile-with-components

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,3 +1,6 @@
+"use client";
+import Button from "@/components/Button"
+import Input from "@/components/Input"
 export default function Profile() {
     return (
         <form className="m-20 mb-10">
@@ -15,18 +18,12 @@ export default function Profile() {
                   <svg className="h-20 w-20 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
                     <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
                   </svg>
-                  <button type="button" className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Change</button>
+                  <Button size={"small"}>Change</Button>
                 </div>
             </div>
 
           <div className="sm:col-span-4">
-            <label for="username" className="block text-sm font-medium leading-6 text-gray-900">Username</label>
-            <div className="mt-2">
-              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">SupremeThrifters.com/</span>
-                <input type="text" name="username" id="username" autocomplete="username" className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="RealKrazyWoman"></input>
-              </div>
-            </div>
+            <Input label={"Username"} placeholder={"RealKrazyWoman"}></Input>
           </div>
   
           <div className="col-span-full">
@@ -52,24 +49,15 @@ export default function Profile() {
         <div className="col-span-8 col-start-6 col-end-12">
         <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div className="sm:col-span-3">
-            <label for="first-name" className="block text-sm font-medium leading-6 text-gray-900">First name</label>
-            <div className="mt-2">
-              <input type="text" name="first-name" id="first-name" autocomplete="given-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
-            </div>
+            <Input label={"First Name"} placeholder={"Real"}></Input>
           </div>
   
           <div className="sm:col-span-3">
-            <label for="last-name" className="block text-sm font-medium leading-6 text-gray-900">Last name</label>
-            <div className="mt-2">
-              <input type="text" name="last-name" id="last-name" autocomplete="family-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
-            </div>
+            <Input label={"Last Name"} placeholder={"KrazyWoman"}></Input>
           </div>
   
           <div className="sm:col-span-4">
-            <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-            <div className="mt-2">
-              <input id="email" name="email" type="email" autocomplete="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
-            </div>
+            <Input label={"Email Address"} placeholder={"realkrazywoman@prnk.com"}></Input>
           </div>
   
           <div className="sm:col-span-3">
@@ -89,31 +77,19 @@ export default function Profile() {
           </div>
   
           <div className="col-span-full">
-            <label for="street-address" className="block text-sm font-medium leading-6 text-gray-900">Street address</label>
-            <div className="mt-2">
-              <input type="text" name="street-address" id="street-address" autocomplete="street-address" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
-            </div>
+            <Input label={"Street Address"} placeholder={"Serangoon North Avenue 3"}></Input>
           </div>
   
           <div className="sm:col-span-2 sm:col-start-1">
-            <label for="city" className="block text-sm font-medium leading-6 text-gray-900">City</label>
-            <div className="mt-2">
-              <input type="text" placeholder=" Singapore" name="city" id="city" autocomplete="address-level2" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
-            </div>
+            <Input label={"Block"} placeholder={"286"}></Input>
           </div>
   
           <div className="sm:col-span-2">
-            <label for="region" className="block text-sm font-medium leading-6 text-gray-900">State / Province</label>
-            <div className="mt-2">
-              <input type="text" name="region" id="region" autocomplete="address-level1" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder=" Singapore"></input>
-            </div>
+            <Input label={"Unit Number"} placeholder={"#12-10"}></Input>
           </div>
   
           <div className="sm:col-span-2">
-            <label for="postal-code" className="block text-sm font-medium leading-6 text-gray-900">Postal code</label>
-            <div className="mt-2">
-              <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
-            </div>
+            <Input label={"Postal Code"} placeholder={"550123"}></Input>
           </div>
         </div>
         </div>
@@ -183,8 +159,8 @@ export default function Profile() {
     </div>
   
     <div className="mt-6 flex items-center justify-end gap-x-6">
-      <button type="button" className="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-      <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+      <Button size={"small"} >Cancel</Button>
+      <Button size={"small"} type={"submit"}>Save</Button>
     </div>
   </form>
 
