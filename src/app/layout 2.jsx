2827fe15bd31d2aace30895b3ce1de53babaed2f
fraser chua <fraser.chua.2022@scis.smu.edu.sg>
2023-10-23 15:05:35 +0800

    rfr

---
+++ src/app/layout 2.jsx
@@ -0,0 +1,31 @@
+import { inter, tiltneon } from "./font";
+import "./globals.css";
+import "@algolia/autocomplete-theme-classic";
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v6.4.2/css/all.css"
+        ></link>
+
+        <link rel="stylesheet" type="text/css" href="style.css"></link>
+
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudfare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
+        ></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&family=Tilt+Neon&display=swap"
+          rel="stylesheet"
+        ></link>
+      </head>
+
+      <body className={tiltneon.className}>{children}</body>
+    </html>
+  );
+}
