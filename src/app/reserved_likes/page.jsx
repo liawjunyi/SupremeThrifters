548dd1dcfb5b1618861ed614c888946ea25f9529
ttroylim <troy.lim.2022@scis.smu.edu.sg>
2023-10-23 10:03:02 +0800

    created reserved/liked page

---
+++ src/app/reserved_likes/page.jsx
@@ -0,0 +1,182 @@
+"use client";
+
+import React, { useState } from 'react';
+
+export default function Reserved() {
+    const products = [
+        {
+          id: 1,
+          name: 'Basic Tee',
+          href: '#',
+          imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+          imageAlt: "Front of men's Basic Tee in black.",
+          price: '$35',
+          color: 'Black',
+        },
+        {
+            id: 1,
+            name: 'Basic Tee',
+            href: '#',
+            imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+            imageAlt: "Front of men's Basic Tee in black.",
+            price: '$35',
+            color: 'Black',
+          },
+        {
+            id: 1,
+            name: 'Basic Tee',
+            href: '#',
+            imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+            imageAlt: "Front of men's Basic Tee in black.",
+            price: '$35',
+            color: 'Black',
+        },
+        {
+            id: 1,
+            name: 'Basic Tee',
+            href: '#',
+            imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+            imageAlt: "Front of men's Basic Tee in black.",
+            price: '$35',
+            color: 'Black',
+        },
+    ]
+
+    const products_likes = [
+        {
+          id: 1,
+          name: 'Basic Tee',
+          href: '#',
+          imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+          imageAlt: "Front of men's Basic Tee in black.",
+          price: '$35',
+          color: 'Black',
+        },
+        {
+            id: 1,
+            name: 'Basic Tee',
+            href: '#',
+            imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+            imageAlt: "Front of men's Basic Tee in black.",
+            price: '$35',
+            color: 'Black',
+          },
+    ]    
+
+    const [selectedTab, setSelectedTab] = useState('reserved');
+
+    const toggleTab = (tab) => {
+    setSelectedTab(tab);
+    };
+  return (
+    <>
+        <header class="bg-gray-900 text-white py-4">
+            <div class="container mx-auto flex justify-between items-center">
+            <h1 class="text-3xl font-semibold px-5">Supreme Thrifters</h1>
+            <nav class="space-x-4 px-5">
+                <a href="#" class="text-white">
+                Home
+                </a>
+                <a href="#" class="text-white">
+                Shop
+                </a>
+                <a href="#" class="text-white">
+                About
+                </a>
+                <a href="#" class="text-white">
+                Contact
+                </a>
+            </nav>
+            </div>
+        </header>
+
+
+        <div className="bg-white">
+            <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+                <div className="flex space-x-4">
+                    <button
+                    onClick={() => toggleTab('reserved')}
+                    className={`${
+                        selectedTab === 'reserved' ? 'bg-black text-white' : 'bg-white text-neutral-500'
+                    } px-4 py-2 rounded-md hover:bg-black hover:text-white focus:outline-none`}
+                    >
+                    Reserved
+                    </button>
+                    <button
+                    onClick={() => toggleTab('liked')}
+                    className={`${
+                        selectedTab === 'liked' ? 'bg-black text-white' : 'bg-white text-neutral-500'
+                    } px-4 py-2 rounded-md hover:bg-black hover:text-white focus:outline-none`}
+                    >
+                    Liked
+                    </button>
+                </div>
+                <div className="tab-content">
+                    {selectedTab === 'reserved' && (
+                    <div>
+                        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                            {products.map((product) => (
+                                <div key={product.id} className="group relative">
+                                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                                        <img
+                                        src={product.imageSrc}
+                                        alt={product.imageAlt}
+                                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                                        />
+                                    </div>
+                                    <div className="mt-4 flex justify-between">
+                                        <div>
+                                            <h3 className="text-sm text-gray-700">
+                                                <a href={product.href}>
+                                                <span aria-hidden="true" className="absolute inset-0" />
+                                                {product.name}
+                                                </a>
+                                            </h3>
+                                            <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                                        </div>
+                                        <p className="text-sm font-medium text-gray-900">{product.price}</p>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    )}
+
+                    {selectedTab === 'liked' && (
+                    <div>
+                        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                            {products_likes.map((product) => (
+                                <div key={product.id} className="group relative">
+                                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                                        <img
+                                        src={product.imageSrc}
+                                        alt={product.imageAlt}
+                                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                                        />
+                                    </div>
+                                    <div className="mt-4 flex justify-between">
+                                        <div>
+                                            <h3 className="text-sm text-gray-700">
+                                                <a href={product.href}>
+                                                <span aria-hidden="true" className="absolute inset-0" />
+                                                {product.name}
+                                                </a>
+                                            </h3>
+                                            <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                                        </div>
+                                        <p className="text-sm font-medium text-gray-900">{product.price}</p>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    )}
+                    
+                </div>
+            </div>
+        </div>
+        
+    </>
+  )
+}
+
