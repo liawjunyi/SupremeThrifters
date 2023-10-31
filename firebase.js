1067d83067bc33c441b226355c2e3a69eab09e89
jonangjj <jonathanang.2022@scis.smu.edu.sg>
2023-11-01 06:34:46 +0800

    authentication for login/signup page

--- firebase.js
+++ firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
 import data from "fakedb.json";
+import {getAuth} from 'firebase/auth';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,6 +23,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
 // const fetch = async () => {
 //   console.log(data);
 //   data.map(async (d) => {
