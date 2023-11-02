68e6503dbc621354878308f4f6a496b7f7135c14
jonangjj <jonathanang.2022@scis.smu.edu.sg>
2023-11-02 17:25:28 +0800

    navbar signout button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,39 @@ import Image from "next/image";
 import Link from "next/link";
 import "@reach/combobox/styles.css";
 import React, { useState, useEffect } from "react";
+import { getAuth, signOut, onAuthStateChanged} from "firebase/auth";
+import {auth} from "../../firebase"
+import Button from "@/components/Button"
+import { data } from "autoprefixer";
 
 const Navbar = () => {
+  
+  
+  const [loggedin, setLoggedin] = useState(true);
+
+  const currentuser = auth.currentUser;
+  console.log("CURRENT USER IS " + currentuser);
+
+  
+
+
+
+  onAuthStateChanged(auth, async (user)=>{
+    if(user){
+        
+        // setLoggedin(true);
+        console.log("signedin status: " + loggedin)
+    }else{
+
+        setLoggedin(false);
+        console.log("Signed out");
+        // console.log(loggedin);
+
+    }
+})
+
+
+
   return (
     <div className="w-full fixed top-0 left-0 bg-primary z-1">
       <div className=" py-5 px-20 lg:flex items-center ">
@@ -47,16 +78,29 @@ const Navbar = () => {
             Profile
           </Link>
 
-          <Link
+          {loggedin?(<Link
+          className="font-semibold text-lg my-7 md:my-0 md:ml-8 text-gray-400 hover:text-neon hover:text-shadow-neon"
+          onClick={() => {signOut(auth)
+            setLoggedin(false)
+            console.log("signin status" + loggedin)
+          }}
+          href="/"
+          >
+           Sign Out  
+          </Link>
+          )
+          :(<Link
             className="font-semibold text-lg my-7 md:my-0 md:ml-8 text-gray-400 hover:text-neon hover:text-shadow-neon"
             href="/login"
           >
             Login
           </Link>
+            )}
         </ul>
       </div>
     </div>
   );
 };
+
 
 export default Navbar;
