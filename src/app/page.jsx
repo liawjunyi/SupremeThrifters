4226c63b80e2d99d2a835eb02ad1a68ee39a8383
ttroylim <troy.lim.2022@scis.smu.edu.sg>
2023-11-03 16:05:17 +0800

    about us portion edited

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -168,10 +168,67 @@ export default function Home () {
                 </div>
               </div>
             ))}
-          </div>
+        </div>
 
-          {/* About Us Section */}
-        <div className="bg-primary text-white text-center h-64 text-xl"> About Us </div>
+        {/* About Us Section */}
+        {/* <div className="bg-primary text-gray-400">
+          <div>
+            <div className="text-center items-center">
+              <h1 className="text-[40px] font-semibold">About Us</h1>
+              <hr className="w-52 h-1.5 bg-primary mx-auto"/>
+            </div>
+            <br></br>
+            <div className="flex">
+              <img src="../../public/tshirt.svg" alt="about us" />
+              <p className="text-center text-l">
+                Supreme Thrifter is created to promote thirfting among youths by making it accessible and convenient for all. Here you can explore and purchase beloved second-hand clothings that is nearest to you rather than having to locate a thrift store which can be very out of the way.
+              </p>
+            </div>
+          </div>
+          <br></br>
+        </div> */}
+
+        <div className="container my-24 mx-auto md:px-6">
+          <section className="mb-32">
+            <div
+              className="block rounded-lg bg-primary shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+              <div className="flex flex-wrap items-center">
+                <div className="hidden shrink-0 grow-0 basis-auto lg:flex lg:w-6/12 xl:w-4/12">
+                  <img src="https://mdbcdn.b-cdn.net/img/new/ecommerce/vertical/088.jpg" alt="Trendy Pants and Shoes"
+                    className="w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg" />
+                </div>
+                <div className="w-full shrink-0 grow-0 basis-auto lg:w-6/12 xl:w-8/12">
+                  <div className="px-6 py-12 md:px-12">
+                    <h2 className="mb-4 text-2xl font-bold text-white">
+                      About Us
+                    </h2>
+                    <p className="mb-6 text-neutral-300 dark:text-neutral-200">
+                      Supreme Thrifter is created to promote thirfting among youths by making it accessible and convenient for all. Here you can explore and purchase beloved second-hand clothings that is nearest to you rather than having to locate a thrift store which can be very out of the way.
+                    </p>
+                    <p className="mb-3 flex items-center font-bold uppercase text-white">
+                      {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                        stroke="currentColor" className="mr-2 h-5 w-5">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
+                      </svg> */}
+                      Did You Know?
+                    </p>
+                    <p className="text-neutral-300 dark:text-neutral-200">
+                      The fashion industry contributes:
+                    </p>
+                    <ul className="text-neutral-300 dark:text-neutral-200">
+                      <li>8-10% of global greenhouse gas emissions.</li>
+                      <li>an estimated 92 million tons of textile waste created annually.</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+        
       </div>
     </>
   );
