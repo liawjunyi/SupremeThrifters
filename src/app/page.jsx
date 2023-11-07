44c2c376c3c5b6742ebc5453d023cb17adf5e743
Toiletspade <jycheang.2022@scis.smu.edu.sg>
2023-11-07 23:50:31 +0800

    Added home Page animation

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,15 +12,7 @@ import Navbar from "@/components/Navbar";
 import Carousel from "@/components/Carousel";
 import Sidemenu from "@/components/Sidemenu";
 import { db } from "../../firebase";
-import {
-  collection,
-  query,
-  where,
-  getDocs,
-  doc,
-  setDoc,
-  updateDoc,
-} from "firebase/firestore";
+import { collection, query, where, getDocs, doc, setDoc, updateDoc } from "firebase/firestore";
 import { useRouter } from "next/navigation";
 import shirt from "../../public/shirt1.jpg";
 import like from "../../public/like.svg";
@@ -30,8 +22,9 @@ export default function Home() {
   const auth = getAuth();
   const user = auth.currentUser;
   const [menuActive, setMenuActive] = useState(false);
+  const [showSideMenu, setShowSideMenu] = useState(false);
   const [all_listing, setAllListings] = useState([]);
-
+  const [selected, setSelected] = useState(null);
   const allListings = async () => {
     const listing = [];
     const querySnapshot = await getDocs(collection(db, "listings"));
@@ -47,14 +40,12 @@ export default function Home() {
     await setDoc(doc(db, `users/${user.uid}/liked`, product.product_name), {
       product,
     });
-    alert(`you liked ${product.product_name}`);
   };
 
   const handleReserved = async (product) => {
     await setDoc(doc(db, `users/${user.uid}/reserved`, product.product_name), {
       product,
     });
-    alert(`you reserved ${product.product_name}`);
   };
 
   useEffect(() => {
@@ -63,9 +54,65 @@ export default function Home() {
 
   const router = useRouter();
   const handleNavigation = (item) => {
-    router.push(`browse?product_id=${item}`);
+    // router.push(`browse?product_id=${item}`);
     console.log(`browse?product_id=${item}`);
-  };
+  }
+  const [isVisibleNewListings, setIsVisibleNewListings] = useState(false);
+  const [isVisibleProduct, setIsVisibleProduct] = useState(Array(8).fill(false));
+  const [isVisibleTrending, setIsVisibleTrending] = useState(false);
+  const [isVisibleTrendingProduct, setIsVisibleTrendingProduct] = useState(Array(8).fill(false));
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const newListingElement = document.getElementById('new-listings-section');
+      const trendingElement = document.getElementById('trending-section');
+      const productElements = document.querySelectorAll('.product-item');
+      const trendingProductElements = document.querySelectorAll('.trending-product-item');
+
+      if (newListingElement) {
+        const rect = newListingElement.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+        const isScrolled = rect.top < windowHeight;
+
+        if (isScrolled) {
+          setIsVisibleNewListings(true);
+          productElements.forEach((element, index) => {
+            setTimeout(() => {
+              setIsVisibleProduct((prev) => {
+                const updatedVisibility = [...prev];
+                updatedVisibility[index] = true;
+                return updatedVisibility;
+              });
+            }, 300 * (index + 1)); // Adjust the delay time as needed for staggered animations
+          });
+        }
+      }
+
+      if (trendingElement) {
+        const rect = trendingElement.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+        const isScrolled = rect.top < windowHeight;
+
+        if (isScrolled) {
+          setIsVisibleTrending(true);
+          trendingProductElements.forEach((element, index) => {
+            setTimeout(() => {
+              setIsVisibleTrendingProduct((prev) => {
+                const updatedVisibility = [...prev];
+                updatedVisibility[index] = true;
+                return updatedVisibility;
+              });
+            }, 300 * (index + 1)); // Adjust the delay time as needed for staggered animations
+          });
+
+          window.removeEventListener('scroll', handleScroll);
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
   //  useEffect(() => {
   //     const updateMediaQuery = (e) => {
   //       if (e.matches) {
@@ -84,7 +131,8 @@ export default function Home() {
   //  }, []);
 
   return (
-    <div className={`w-full ${menuActive ? "h-screen overflow-hidden" : ""}`}>
+    <div className={`max-w-full ${menuActive ? "h-screen overflow-hidden" : ""}`}>
+      
       <Sidemenu
         className={`transition-opacity duration-500 ${
           menuActive ? "opacity-100 ease-in z-20" : "opacity-0 ease-out z-0"
@@ -92,27 +140,25 @@ export default function Home() {
         onClick={() => setMenuActive((prev) => !prev)}
       />
       <Navbar menuActive={menuActive} setMenuActive={setMenuActive} />
-
+      
       <div className="max-w-full ">
         <div className="mt-20"></div>
         <Carousel />
 
         {/* New Listings section */}
-        <div className="text-center items-center">
-          <h1 className="text-[40px] font-semibold">New Listings</h1>
-          <hr className="w-52 h-1.5 bg-primary mx-auto" />
-        </div>
-        <div className="mt-6 mb-6 mx-11 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {all_listing.slice(0, 8).map((product) => (
-            <div
-              key={product.id}
-              className="group relative justify-evenly flex"
-            >
-              <div
-                onClick={() => {
-                  handleNavigation(product.product_id);
-                }}
-              >
+        <div id="new-listings-section" className="text-center items-center mt-6 mb-6 mx-11">
+      <h1 className={`text-[40px] font-semibold ${isVisibleNewListings ? 'opacity-100 animate-fade-in-left duration-3000' : 'opacity-0'}`}>
+        New Listings
+      </h1>
+      <hr className={`w-52 h-1.5 bg-primary mx-auto ${isVisibleNewListings ? 'opacity-100 animate-fade-in-left duration-3000' : 'opacity-0'}`} />
+    </div>
+    <div className="mt-6 mb-6 mx-11 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+      {all_listing.slice(0, 8).map((product, index) => (
+        <div
+          key={product.id}
+          className={`product-item group relative justify-evenly flex transition-all transform ${isVisibleProduct[index] ? 'opacity-100 animate-fade-in-left duration-3000' : 'opacity-0'}`}
+        >
+              <div onClick={()=>{handleNavigation(product.product_id)}}>
                 <Card className="aspect-h-1 aspect-w-1 w-[300px] overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-90">
                   <img
                     src={product.product_img_url}
@@ -159,33 +205,31 @@ export default function Home() {
                   </div> */}
                 </Card>
                 <div className="flex justify-between">
-                  <Button
-                    className="z-0"
-                    size="sm"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      
-                      handleReserved(product);
-                      console.log("reserved");
-                    }}
-                    animation="animate-bounce"
-                  >
-                    Reserve
-                  </Button>
-                  <Button
-                    className="z-0"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      handleLiked(product);
-                      console.log("liked");
-                    }}
-                    size="sm"
-                    bold={true}
-                    animation="animate-bounce"
-                  >
-                    <Image src={like} />
-                  </Button>
-                </div>
+                            <Button
+                            className="z-0"
+                              size="sm"
+                              onClick={(e) => {
+                                e.stopPropagation();
+                                
+                                handleReserved(product);
+                                console.log("reserved");
+                              }}
+                            >
+                              Reserve
+                            </Button>
+                            <Button
+                            className="z-0"
+                              onClick={(e) => {
+                                e.stopPropagation();
+                                handleLiked(product);
+                                console.log("liked");
+                              }}
+                              size="sm"
+                              bold={true}
+                            >
+                              <Image src={like} />
+                            </Button>
+                          </div>
                           
               </div>
             </div>
@@ -193,21 +237,19 @@ export default function Home() {
         </div>
 
         {/* New Listings section */}
-        <div className="text-center items-center">
-          <h1 className="text-[40px] font-semibold">Trendings</h1>
-          <hr className="w-52 h-1.5 bg-primary mx-auto" />
-        </div>
-        <div className="mt-6 mb-6 mx-11 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 ">
-          {all_listing.slice(9, 17).map((product) => (
-            <div
-              key={product.id}
-              className="group relative justify-evenly flex"
-            >
-              <div
-                onClick={() => {
-                  handleNavigation(product.product_id);
-                }}
-              >
+        <div id="trending-section" className="text-center items-center mt-6 mb-6 mx-11">
+      <h1 className={`text-[40px] font-semibold ${isVisibleTrending ? 'opacity-100 animate-fade-in-left duration-3000' : 'opacity-0'}`}>
+        Trending
+      </h1>
+      <hr className={`w-52 h-1.5 bg-primary mx-auto ${isVisibleTrending ? 'opacity-100 animate-fade-in-left duration-3000' : 'opacity-0'}`} />
+    </div>
+    <div className="mt-6 mb-6 mx-11 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+      {all_listing.slice(9, 17).map((product, index) => (
+        <div
+          key={product.id}
+          className={`trending-product-item group relative justify-evenly flex transition-all transform ${isVisibleTrendingProduct[index] ? 'opacity-100 animate-fade-in-left duration-3000' : 'opacity-0'}`}
+        >
+              <div onClick={()=>{handleNavigation(product.product_id)}}>
                 <Card className="aspect-h-1 aspect-w-1 w-[300px] overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-90">
                   <img
                     src={product.product_img_url}
@@ -234,46 +276,38 @@ export default function Home() {
                   </div>
                 </Card>
                 <div className="flex justify-between">
-                  <Button
-                    className="z-0"
-                    size="sm"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      
-                      handleReserved(product);
-                      console.log("reserved");
-                      animation="animate-bounce"
-                    }}
-                    animation="animate-bounce"
-                  >
-                    Reserve
-                  </Button>
-                  <Button
-                    className="z-0"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      handleLiked(product);
-                      console.log("liked");
-                    }}
-                    size="sm"
-                    bold={true}
-                    animation="animate-bounce"
-                  >
-                    <Image src={like} />
-                  </Button>
-                </div>
+                            <Button
+                            className="z-0"
+                              size="sm"
+                              onClick={(e) => {
+                                e.stopPropagation();
+                                
+                                handleReserved(product);
+                                console.log("reserved");
+                              }}
+                            >
+                              Reserve
+                            </Button>
+                            <Button
+                            className="z-0"
+                              onClick={(e) => {
+                                e.stopPropagation();
+                                handleLiked(product);
+                                console.log("liked");
+                              }}
+                              size="sm"
+                              bold={true}
+                            >
+                              <Image src={like} />
+                            </Button>
+                          </div>
               </div>
             </div>
           ))}
         </div>
 
         {/* About Us Section */}
-        <div
-          onClick={() => {
-            handleNavigation(product.product_id);
-          }}
-          className="m-4 mx-8 mb-10 "
-        >
+        <div onClick={()=>{handleNavigation(product.product_id)}} className="m-4 mx-8 mb-10 ">
           <Card2 image={shirt} title="About us">
             <p class="mb-6 text-neutral-300 dark:text-neutral-200 text-lg">
               Supreme Thrifter is created to promote thirfting among youths by
@@ -314,7 +348,8 @@ export default function Home() {
           © 2023 Copyright: Supreme Thrifters
         </div>
       </footer>
-    </div>
+      </div>
+    
   );
 }
 
